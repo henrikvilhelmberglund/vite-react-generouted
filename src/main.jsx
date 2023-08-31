1d5ce560a13663da7802e4fa33b6184f5e70b230
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-08-31 17:38:14 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./main.css";
+import "virtual:uno.css";
+import { Routes } from "@generouted/react-router";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Routes />
+  </React.StrictMode>
+);
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import "./index.css";
+import { useState } from "react";
+export default function Home() {
+  return (
+    <div className="App">
+      <h1>Generouted</h1>
+      <h2>It works!</h2>
+      <Link to="/cool-route">Cool route here</Link>
+    </div>
+  );
+}
