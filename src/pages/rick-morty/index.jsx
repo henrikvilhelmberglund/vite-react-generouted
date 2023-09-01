8dfb91bc0c3326ecc3cea8c1a3ca912443145b1b
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-01 22:50:46 +0200

    feat: :sparkles: add images, better formatting for text

--- src/pages/rick-morty/index.jsx
+++ src/pages/rick-morty/index.jsx
@@ -51,92 +51,100 @@ export default function Index() {
     <>
       <h1>Rick & Morty - How many characters</h1>
 
-      <main className="flex flex-col mx-auto w-64 [&>*]:m-2 [&>*]:p-2">
-        <input
-          onBlur={(e) => {
-            setSelectedName(e.target.value);
-            setShouldFetch(true);
-          }}
-          type="text"
-          placeholder="Name"
-          onKeyDown={(e) => {
-            if (e.code === "Enter") {
+      <main className="flex flex-col mx-auto  ">
+        <div className="flex flex-col mx-auto w-64 [&>*]:m-2 [&>*]:p-2">
+          <input
+            onBlur={(e) => {
               setSelectedName(e.target.value);
-            }
-          }}
-        />
-        <select
-          onChange={(e) => {
-            setSelectedGender(e.target.value);
-            setShouldFetch(true);
-          }}
-          name=""
-          id=""
-        >
-          {genders.map((gender, i) => (
-            <option key={i} value={gender}>
-              {gender}
-            </option>
-          ))}
-        </select>
-        <select
-          onChange={(e) => {
-            setSelectedSpecies(e.target.value);
-            setShouldFetch(true);
-          }}
-          name=""
-          id=""
-        >
-          {species.map((specie, i) => (
-            <option key={i} value={specie}>
-              {specie}
-            </option>
-          ))}
-        </select>
-        <select
-          onChange={(e) => {
-            setSelectedStatus(e.target.value);
-            setShouldFetch(true);
-          }}
-          name=""
-          id=""
-        >
-          {statuses.map((status, i) => (
-            <option key={i} value={status}>
-              {status}
-            </option>
-          ))}
-        </select>
+              setShouldFetch(true);
+            }}
+            type="text"
+            placeholder="Name"
+            onKeyDown={(e) => {
+              if (e.code === "Enter") {
+                setSelectedName(e.target.value);
+              }
+            }}
+          />
+          <select
+            onChange={(e) => {
+              setSelectedGender(e.target.value);
+              setShouldFetch(true);
+            }}
+            name=""
+            id=""
+          >
+            {genders.map((gender, i) => (
+              <option key={i} value={gender}>
+                {gender}
+              </option>
+            ))}
+          </select>
+          <select
+            onChange={(e) => {
+              setSelectedSpecies(e.target.value);
+              setShouldFetch(true);
+            }}
+            name=""
+            id=""
+          >
+            {species.map((specie, i) => (
+              <option key={i} value={specie}>
+                {specie}
+              </option>
+            ))}
+          </select>
+          <select
+            onChange={(e) => {
+              setSelectedStatus(e.target.value);
+              setShouldFetch(true);
+            }}
+            name=""
+            id=""
+          >
+            {statuses.map((status, i) => (
+              <option key={i} value={status}>
+                {status}
+              </option>
+            ))}
+          </select>
+          <button
+            onClick={() => {
+              fetchResult();
+            }}
+          >
+            Get characters
+          </button>
+        </div>
         <p>
           You're looking for {selectedName ? `someone named ${selectedName}` : `anyone`} with
           {selectedGender === "All" ? " any gender" : ` a ${selectedGender} gender`} of
           {selectedSpecies === "All" ? " any species" : ` a ${selectedSpecies} species`} with
           {selectedStatus === "All" ? " any status " : ` the status ${selectedStatus}`}
         </p>
-        <button
-          onClick={() => {
-            fetchResult();
-          }}
-        >
-          Get characters
-        </button>
         {result?.info?.count && (
           <p className="text-lg font-semibold">
             {result?.info?.count} matching {result?.info?.count > 1 ? "characters" : "character"}
           </p>
         )}
         {result?.error && <p>No characters were found.</p>}
-        {result?.results?.map(
-          ({ name, species, gender, type, origin: { name: originName } }, i) => (
-            <article className="outline-green-300 outline-1 outline-solid rounded-xl" key={i}>
-              <p className="font-semibold">{name}</p>
-              <p className="">{species}</p>
-              <p className="">{gender}</p>
-              <p className="">{type}</p>
-              <p className="">{originName}</p>
-            </article>
-          )
-        )}
+        <section className="flex flex-wrap justify-start w-[70vw] mx-auto">
+          {result?.results?.map(
+            ({ name, species, gender, type, image, origin: { name: originName } }, i) => (
+              <article
+                className="outline-black outline-1 outline-solid rounded-xl min-w-[300px] min-h-[300px] m-2"
+                key={i}
+              >
+                <img className="relative left-0 rounded-xl" src={image}></img>
+                <p className="font-semibold">{name}</p>
+                <span className="">
+                  {species} {gender.toLowerCase()} {type.toLowerCase()}
+                </span>
+                <p className="">{originName}</p>
+              </article>
+            )
+          )}
+        </section>
       </main>
     </>
   );
