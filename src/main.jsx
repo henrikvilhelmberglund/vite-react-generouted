33102c18509c6898902777d451e2673f4abc3211
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-04 15:52:04 +0200

    fix: :bug: fix generouted caching bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
 import "virtual:uno.css";
-import { Routes } from "@generouted/react-router";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routes } from "generouted/react-router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Routes />
+    <RouterProvider router={createBrowserRouter(routes)} />
   </React.StrictMode>
 );
